70c3f9becc62ba1ce911fd33b3ff2cd4f17e45a2
Tom <noskap@outlook.com>
2024-02-26 16:18:09 +1000

    Rename base class

--- lib/lint_plugin_todo.dart
+++ lib/lint_plugin_todo.dart
@@ -6,10 +6,10 @@ import 'package:analyzer/src/dart/ast/ast.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
 // This is the entrypoint of our custom linter
-PluginBase createPlugin() => _ExampleLinter();
+PluginBase createPlugin() => _TodoLinter();
 
 /// A plugin class is used to list all the assists/lints defined by a plugin.
-class _ExampleLinter extends PluginBase {
+class _TodoLinter extends PluginBase {
   /// We list all the custom warnings/infos/errors
   @override
   List<LintRule> getLintRules(CustomLintConfigs configs) => [
